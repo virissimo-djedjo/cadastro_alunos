5668b275174998f5fad8e37cd58a6b0e315ea961
Virissimo Djedjo <djedjovirissimo450@gmail.com>
2026-07-31 20:08:06 -0300

    fix: cria rota de deletar alunos e atualizar aluno

--- backend/alunos.js
+++ backend/alunos.js
@@ -1,9 +1,9 @@
 import "dotenv/config";
 import pool from "./conexao.js";
 import { Router } from "express";
-import bcrypt from "bcrypt";
+import bcrypt, { genSalt } from "bcrypt";
 import jwt from "jsonwebtoken";
-import {autenticar} from "./middlewares.js"
+import { autenticar } from "./middlewares.js";
 
 const router = Router();
 
@@ -11,14 +11,16 @@ router.post("/cadastrar", async (req, res) => {
   try {
     const { nome, email, password } = req.body;
 
-    if(!nome || !email || !password){
-      return res.json('Campos nome, email e password são obrigatórios');
+    if (!nome || !email || !password) {
+      return res.json("Campos nome, email e password são obrigatórios");
     }
 
-    const jaExiste = await pool.query("select * from alunos where email = $1", [email]);
+    const jaExiste = await pool.query("select * from alunos where email = $1", [
+      email,
+    ]);
 
-    if(jaExiste.rowCount > 0){
-      return res.status(400).json('Este email já existe');
+    if (jaExiste.rowCount > 0) {
+      return res.status(400).json("Este email já existe");
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -28,10 +30,14 @@ router.post("/cadastrar", async (req, res) => {
       "INSERT INTO alunos (nome, email, password) VALUES ($1,$2,$3) RETURNING id, nome, email, password",
       [nome, email, senhaHashada],
     );
-    return res.status(201).json({ mensagem: "Usuario cadastro com sucesso", data: dados.rows });
+    return res
+      .status(201)
+      .json({ mensagem: "Usuario cadastro com sucesso", data: dados.rows });
   } catch (erro) {
     console.error(erro.message);
-    return res.status(500).json({mensagem: 'erro ao cadastrar aluno', erro: erro});
+    return res
+      .status(500)
+      .json({ mensagem: "erro ao cadastrar aluno", erro: erro });
   }
 });
 
@@ -39,8 +45,8 @@ router.post("/login", async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    if(!email || !password){
-      return res.json('email e password são obrigatórios')
+    if (!email || !password) {
+      return res.json("email e password são obrigatórios");
     }
 
     const aluno = await pool.query("select * from alunos where email = $1", [
@@ -52,7 +58,7 @@ router.post("/login", async (req, res) => {
     }
 
     const usuario = aluno.rows[0];
-    
+
     const senhaValida = await bcrypt.compare(password, usuario.password);
 
     if (!senhaValida) {
@@ -65,38 +71,107 @@ router.post("/login", async (req, res) => {
       { expiresIn: "7d" },
     );
 
-    return res.status(200).json({token: token, dados:{ id: usuario.id, email: usuario.email} });
-
+    return res
+      .status(200)
+      .json({ token: token, dados: { id: usuario.id, email: usuario.email } });
   } catch (erro) {
-    console.error(erro.message)
-    return res.status(500).json('erro ao realizar login')
+    console.error(erro.message);
+    return res.status(500).json("erro ao realizar login");
   }
 });
 
+router.get("/perfil", autenticar, async (req, res) => {
+  const aluno = await pool.query("SELECT * FROM alunos where email = $1", [
+    req.usuario.email,
+  ]);
 
-router.get("/perfil", autenticar, async (req, res)=>{
+  res.json(aluno.rows);
+});
 
-  const aluno = await pool.query('SELECT * FROM alunos where email = $1', [req.usuario.email])
-
-
-  res.json(aluno.rows)
-})
-
-router.get("/alunos", async (req, res)=>{
+router.get("/alunos", async (req, res) => {
   try {
-    const aluno = await pool.query("SELECT id, nome, email FROM alunos")
-    if(aluno.rowCount === 0){
-      return res.status(400).json({message: "Nenhum aluno encontrado"})
+    const aluno = await pool.query("SELECT id, nome, email FROM alunos");
+    if (aluno.rowCount === 0) {
+      return res.status(400).json({ message: "Nenhum aluno encontrado" });
     }
 
-  res.status(200).json({dados: aluno.rows})
+    res.status(200).json({ dados: aluno.rows });
   } catch (erro) {
-    console.log(erro.message)
+    console.log(erro.message);
 
-    return res.status(500).json({message: "Erro ao procurar alunos"})
+    return res.status(500).json({ message: "Erro ao procurar alunos" });
+  }
+});
+
+router.get("/alunos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const aluno = await pool.query("SELECT id, nome, email FROM alunos where id = $1", [id]);
+
+    if (aluno.rowCount === 0) {
+      return res.status(400).json({ message: "Nenhum aluno encontrado" });
+    }
+
+    res.status(200).json({ dados: aluno.rows });
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({ message: "Erro ao procurar alunos" });
+  }
+});
+
+router.delete("/alunos/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const aluno = await pool.query("DELETE FROM alunos where id = $1", [id]);
+
+    if (aluno.rowCount === 0) {
+      return res.status(400).json({ message: "Nenhum aluno encontrado" });
+    }
+
+    res.status(200).json({ message: "Aluno deletado com sucesso"});
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({ erro: erro.message});
+  }
+});
+
+router.put("/alunos/:id", async (req, res)=>{
+   
+  const { id } = req.params
+  const { nome, email, password } = req.body
+
+  try {
+
+    const alunoId = await pool.query("SELECT id, nome, email FROM alunos where id = $1", [id]);
+
+    if (alunoId.rowCount === 0) {
+      return res.status(400).json({ message: "Nenhum aluno encontrado" });
+    }
+
+    const salt = await genSalt(10)
+
+    const senhaHashada = await bcrypt.hash(password, salt)
+
+    const alunoExiste = await pool.query("SELECT * FROM alunos where email = $1", [email])
+
+    if(alunoExiste.rowCount > 0){
+      return res.status(401).json("Esse email ja existe ")
+    }
+
+    const aluno = await pool.query("UPDATE alunos SET nome = $1, email = $2, password = $3 where id = $4 RETURNING id, nome, email", [
+      nome, email, senhaHashada, id
+    ])
+
+    res.status(200).json({dados: aluno.rows})
+  } catch (erro) {
+    console.log(erro.message);
+
+    return res.status(500).json({ erro: erro.message});
   }
 })
-
-
 
 export default router;
